3c2a32fbba4e23a7ed2cce9fb3c9c35083a6daff
Taewon Kong <tkong9@gatech.edu>
2019-09-14 01:48:19 -0400

    complete part1

--- Solo Mission/GameViewController.swift
+++ Solo Mission/GameViewController.swift
@@ -12,24 +12,51 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//
+//            view.ignoresSiblingOrder = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
+//    }
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        
+
         if let view = self.view as! SKView? {
+
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+
+            let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+
+            // Set the scale mode to scale to fit the window
+
+            scene.scaleMode = .aspectFill
+
+            // Present the scene
+
+            view.presentScene(scene)
+
             view.ignoresSiblingOrder = true
-            
+
             view.showsFPS = true
+
             view.showsNodeCount = true
         }
+
     }
 
     override var shouldAutorotate: Bool {
